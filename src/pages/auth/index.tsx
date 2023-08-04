7f9cc6d4a171843db533ae2cf4f3c43dfc97102a
Patrick Kim <patrick.yrkim@gmail.com>
2023-08-03 17:17:33 -0700

    Add auth page

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Navbar from '@/components/Navbar/Navbar';
+
+type AuthPageProps = {};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+    return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+        <div className='max-w-7xl mx-auto'>
+            <Navbar />
+            <div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
+                <img src="/hero.png" alt="hero img" />
+            </div>
+        </div>
+    </div>
+}
+export default AuthPage;
