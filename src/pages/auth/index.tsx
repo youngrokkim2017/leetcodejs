1f91a888d7bae4c86a60150275ace7e86f7e9f85
Patrick Kim <patrick.yrkim@gmail.com>
2023-08-08 00:19:11 -0700

    Add auth page loading and user check

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/firebase';
+import { useRouter } from 'next/router';
 
 import Navbar from '@/components/Navbar/Navbar';
 import AuthModal from '@/components/Modals/AuthModal';
@@ -9,6 +12,16 @@ type AuthPageProps = {};
 
 const AuthPage:React.FC<AuthPageProps> = () => {
     const authModal = useRecoilValue(authModalState)
+    const [user, loading, error] = useAuthState(auth)
+    const router = useRouter()
+    const [pageLoading, setPageLoading] = useState(true)
+
+    useEffect(() => {
+		if (user) router.push("/")
+		if (!loading && !user) setPageLoading(false)
+	}, [user, router, loading])
+
+    if (pageLoading) return null
 
     return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
         <div className='max-w-7xl mx-auto'>
