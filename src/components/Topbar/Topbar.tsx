3629cae0a269e089128ac618ddb99e3e59dfc664
Patrick Kim <patrick.yrkim@gmail.com>
2023-08-09 17:24:24 -0700

    Update topbar with auth conditions

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,9 +1,13 @@
+import { auth } from '@/firebase/firebase';
 import Link from 'next/link';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 type TopbarProps = {};
 
 const Topbar:React.FC<TopbarProps> = () => {
+    const [user] = useAuthState(auth)
+
     return (
         <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
 			<div className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}>
@@ -22,9 +26,22 @@ const Topbar:React.FC<TopbarProps> = () => {
 							Premium
 						</a>
 					</div>
-					<Link href='/auth'>
-						<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
-					</Link>
+                    {!user && (
+                        <Link href='/auth'>
+                            <button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
+                        </Link>
+                    )}
+                    {user && (
+                        <div className='cursor-pointer group relative'>
+                            <img src='/avatar.png' alt='user profile img' className='h-8 w-8 rounded-full' />
+                            <div
+	                        	className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 
+                                group-hover:scale-100 scale-0 transition-all duration-300 ease-in-out'
+	                        >
+	                        	<p className='text-sm'>{user.email}</p>
+	                        </div>
+                        </div>
+                    )}
 				</div>
 			</div>
         </nav>
