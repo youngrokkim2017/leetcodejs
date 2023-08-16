20b00b9dc9a2e82aaeb15bbebf25a338aebf9d62
Patrick Kim <patrick.yrkim@gmail.com>
2023-08-15 21:18:55 -0700

    Add preference nav component

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Split from 'react-split';
 
 import ProblemDescription from './ProblemDescription/ProblemDescription';
+import Playground from './Playground/Playground';
 
 type WorkspaceProps = {};
 
 const Workspace:React.FC<WorkspaceProps> = () => {
     return <Split className='split'>
         <ProblemDescription />
-        <div>Code Editor Placeholder</div>
+        <Playground />
     </Split>
 }
 export default Workspace;
