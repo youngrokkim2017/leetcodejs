14cc41d6ea2250563b172478c7cf59b585e14d94
Patrick Kim <patrick.yrkim@gmail.com>
2023-08-16 18:45:16 -0700

    Add test cases template

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -21,8 +21,41 @@ const Playground:React.FC<PlaygroundProps> = () => {
                         style={{fontSize:16}}
                     />
                 </div>
-                <div>
-                    test cases
+                <div className='w-full px-5 overflow-auto'>
+                    {/* test case heading */}
+                    <div className='flex h-10 items-center space-x-6'>
+                        <div className='relative flex h-full flex-col justify-center cursor-pointer'>
+                            <div className='text-sm font-medium leading-5 text-white'>
+                                Test Cases
+                            </div>
+                            <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none' />
+                        </div>
+                    </div>
+
+                    <div className='flex'>
+                        {/* case 1 */}
+                        <div className='mr-2 items-start mt-2 text-white'>
+                            <div className='flex flex-wrap items-center gap-y-4'>
+                                <div 
+                                    className='font-medium items-center transition-all focus:outline-none inline-flex 
+                                    bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'
+                                >
+                                    case 1
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='font-semibold my-4'>
+						<p className='text-sm font-medium mt-4 text-white'>Input:</p>
+						<div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+							nums: [2, 7, 11, 15], target: 9
+						</div>
+						<p className='text-sm font-medium mt-4 text-white'>Output:</p>
+						<div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+							[0, 1]
+						</div>
+					</div>
                 </div>
             </Split>
         </div>
