c6d325fbe7d7a435d18f4033972182e18fd8f109
Patrick Kim <patrick.yrkim@gmail.com>
2023-09-01 16:05:53 -0700

    Add temp form to add problems

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
+import { firestore } from "@/firebase/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { useState } from "react";
 
 export default function Home() {
+  const [inputs, setInputs] = useState({
+    id: '',
+    title: '',
+    difficulty: '',
+    category: '',
+    videoId: '',
+    link: '',
+    order: 0,
+    likes: 0,
+    dislikes: 0,
+  })
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // conver inputs.order to integer
+    const newProblem = {
+      ...inputs,
+      // order: parseInt(inputs.order),
+      order: Number(inputs.order)
+    }
+    await setDoc(doc(firestore, "problems", inputs.id), newProblem)
+    alert("saved to db")
+  }
+
   return (
     <>
       <main className="bg-dark-layer-2 min-h-screen">
@@ -35,6 +69,18 @@ export default function Home() {
             <ProblemsTable />
           </table>
         </div>
+
+        {/* temo form */}
+        <form className="p-6 tlex flex0col max-w-sm gap-3" onSubmit={handleSubmit}>
+          <input onChange={handleInputChange} type="text" placeholder="problem id" name="id" />
+          <input onChange={handleInputChange} type="text" placeholder="title" name="title" />
+          <input onChange={handleInputChange} type="text" placeholder="difficulty" name="difficulty" />
+          <input onChange={handleInputChange} type="text" placeholder="category" name="category" />
+          <input onChange={handleInputChange} type="text" placeholder="order" name="order" />
+          <input onChange={handleInputChange} type="text" placeholder="videoId?" name="videoId" />
+          <input onChange={handleInputChange} type="text" placeholder="link?" name="link" />
+          <button className="bg-white">save to database</button>
+        </form>
       </main>
     </>
   )
