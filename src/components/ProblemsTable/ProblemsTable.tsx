e2c2067f26a976e9bf396567b2e75d2a13238c01
Patrick Kim <patrick.yrkim@gmail.com>
2023-09-02 17:38:55 -0700

    Add fetch problem functionality

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -4,16 +4,24 @@ import { AiFillYoutube } from 'react-icons/ai';
 import Link from 'next/link';
 import { IoClose } from 'react-icons/io5';
 import { YouTubePlayer } from 'react-youtube';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
 
-import { problems } from '@/mockProblems/problems';
+// import { problems } from '@/mockProblems/problems';
+import { firestore } from '@/firebase/firebase';
+import { DBProblem } from '@/utils/types/problem';
 
-type ProblemsTableProps = {};
+type ProblemsTableProps = {
+    setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>
+};
 
-const ProblemsTable:React.FC<ProblemsTableProps> = () => {
+const ProblemsTable:React.FC<ProblemsTableProps> = ({ setLoadingProblems }) => {
     const [youtubePlayer, setYoutubePlayer] = useState({
         isOpen: false,
         videoId: '',
     })
+
+    //  fetch problems
+    const problems = useGetProblems(setLoadingProblems)
 
     const closeModal = () => {
 		setYoutubePlayer({ isOpen: false, videoId: "" })
@@ -31,37 +39,43 @@ const ProblemsTable:React.FC<ProblemsTableProps> = () => {
     return (
         <>
             <tbody className='text-white'>
-                {problems.map((doc, idx) => {
+                {problems.map((problem, idx) => {
                     const difficultyColor = 
-                        doc.difficulty === "Easy"
+                        problem.difficulty === "Easy"
 		    					? "text-dark-green-s"
-		    					: doc.difficulty === "Medium"
+		    					: problem.difficulty === "Medium"
 		    					? "text-dark-yellow"
 		    					: "text-dark-pink";
 
                     return (
-                        <tr className={`${idx % 2 === 1 ? 'bg-dark-layer-1' : ''}`} key={doc.id}>
+                        <tr className={`${idx % 2 === 1 ? 'bg-dark-layer-1' : ''}`} key={problem.id}>
                             <th className='px-2 py-4 font-medium whitespace-nowrap text-dark-green-s'> 
                                 <BsCheckCircle fontSize="18" width="18" />
                             </th>
                             <td className='px-6 py-4'>
-                                <Link className="hover:text-blue-600 cursor-pointer" href={`/problems/${doc.id}`}>
-                                    {doc.title}
-                                </Link>
+                                {problem.link ? (
+                                    <Link href={problem.link} className='hover:text-blue-600 cursor-pointer' target='blank'>
+                                        {problem.title}
+                                    </Link>
+                                ) : (
+                                    <Link className="hover:text-blue-600 cursor-pointer" href={`/problems/${problem.id}`}>
+                                        {problem.title}
+                                    </Link>
+                                )}
                             </td>
                             <td className={`px-6 py-4 ${difficultyColor}`}>
-                                {doc.difficulty}
+                                {problem.difficulty}
                             </td>
                             <td className='px-6 py-4'>
-                                {doc.category}
+                                {problem.category}
                             </td>
                             <td className='px-6 py-4'>
-                                {doc.videoId ? (
+                                {problem.videoId ? (
                                     <AiFillYoutube 
                                         fontSize={"28"}
 										className='cursor-pointer hover:text-red-600'
 										onClick={() =>
-											setYoutubePlayer({ isOpen: true, videoId: doc.videoId as string })
+											setYoutubePlayer({ isOpen: true, videoId: problem.videoId as string })
 										}
                                     />
                                 ) : (
@@ -100,3 +114,25 @@ const ProblemsTable:React.FC<ProblemsTableProps> = () => {
     )
 }
 export default ProblemsTable;
+
+function useGetProblems(setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>) {
+	const [problems, setProblems] = useState<DBProblem[]>([]);
+
+	useEffect(() => {
+		const getProblems = async () => {
+			// fetching data logic
+			setLoadingProblems(true);
+			const q = query(collection(firestore, "problems"), orderBy("order", "asc"));
+			const querySnapshot = await getDocs(q);
+			const tmp: DBProblem[] = [];
+			querySnapshot.forEach((doc) => {
+				tmp.push({ id: doc.id, ...doc.data() } as DBProblem);
+			});
+			setProblems(tmp);
+			setLoadingProblems(false);
+		};
+
+		getProblems();
+	}, [setLoadingProblems]);
+	return problems;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,54 @@
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/Topbar";
-import { firestore } from "@/firebase/firebase";
-import { doc, setDoc } from "firebase/firestore";
+// import { firestore } from "@/firebase/firebase";
+// import { doc, setDoc } from "firebase/firestore";
 import { useState } from "react";
 
+const LoadingSkeleton = () => {
+	return (
+		<div className='flex items-center space-x-12 mt-4 px-6'>
+			<div className='w-6 h-6 shrink-0 rounded-full bg-dark-layer-1'></div>
+			<div className='h-4 sm:w-52  w-32  rounded-full bg-dark-layer-1'></div>
+			<div className='h-4 sm:w-52  w-32 rounded-full bg-dark-layer-1'></div>
+			<div className='h-4 sm:w-52 w-32 rounded-full bg-dark-layer-1'></div>
+			<span className='sr-only'>Loading...</span>
+		</div>
+	);
+};
+
 export default function Home() {
-  const [inputs, setInputs] = useState({
-    id: '',
-    title: '',
-    difficulty: '',
-    category: '',
-    videoId: '',
-    link: '',
-    order: 0,
-    likes: 0,
-    dislikes: 0,
-  })
+  const [loadingProblems, setLoadingProblems] = useState(true)
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputs({
-      ...inputs,
-      [e.target.name]: e.target.value
-    })
-  }
+  // const [inputs, setInputs] = useState({
+  //   id: '',
+  //   title: '',
+  //   difficulty: '',
+  //   category: '',
+  //   videoId: '',
+  //   link: '',
+  //   order: 0,
+  //   likes: 0,
+  //   dislikes: 0,
+  // })
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    // conver inputs.order to integer
-    const newProblem = {
-      ...inputs,
-      // order: parseInt(inputs.order),
-      order: Number(inputs.order)
-    }
-    await setDoc(doc(firestore, "problems", inputs.id), newProblem)
-    alert("saved to db")
-  }
+  // const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setInputs({
+  //     ...inputs,
+  //     [e.target.name]: e.target.value
+  //   })
+  // }
+
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault()
+  //   // conver inputs.order to integer
+  //   const newProblem = {
+  //     ...inputs,
+  //     // order: parseInt(inputs.order),
+  //     order: Number(inputs.order)
+  //   }
+  //   await setDoc(doc(firestore, "problems", inputs.id), newProblem)
+  //   alert("saved to db")
+  // }
 
   return (
     <>
@@ -45,33 +59,42 @@ export default function Home() {
           &ldquo; QUALITY OVER QUANTITY &rdquo; 👇
         </h1>
         <div className='relative overflow-x-auto mx-auto px-6 pb-10'>
+          {loadingProblems && (
+            <div className="animate-pulse max-w-[1200px] mx-auto sm:w-7/12 w-full">
+              {[...Array(10)].map((_, idx) => (
+                <LoadingSkeleton key={idx} />
+              ))}
+            </div>
+          )}
           <table className='text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto'>
-            <thead className='text-xs text-gray-700 uppercase dark:text-gray-400 border-b '>
-              <tr>
-                <th scope='col' className='px-1 py-3 w-0 font-medium'>
-                  Status
-                </th>
-                <th scope='col' className='px-6 py-3 w-0 font-medium'>
-                  Title
-                </th>
-                <th scope='col' className='px-6 py-3 w-0 font-medium'>
-                  Difficulty
-                </th>
+            {!loadingProblems && (  
+              <thead className='text-xs text-gray-700 uppercase dark:text-gray-400 border-b '>
+                <tr>
+                  <th scope='col' className='px-1 py-3 w-0 font-medium'>
+                    Status
+                  </th>
+                  <th scope='col' className='px-6 py-3 w-0 font-medium'>
+                    Title
+                  </th>
+                  <th scope='col' className='px-6 py-3 w-0 font-medium'>
+                    Difficulty
+                  </th>
 
-                <th scope='col' className='px-6 py-3 w-0 font-medium'>
-                  Category
-                </th>
-                <th scope='col' className='px-6 py-3 w-0 font-medium'>
-                  Solution
-                </th>
-              </tr>
-            </thead>
-            <ProblemsTable />
+                  <th scope='col' className='px-6 py-3 w-0 font-medium'>
+                    Category
+                  </th>
+                  <th scope='col' className='px-6 py-3 w-0 font-medium'>
+                    Solution
+                  </th>
+                </tr>
+              </thead>
+            )}
+            <ProblemsTable setLoadingProblems={setLoadingProblems} />
           </table>
         </div>
 
         {/* temo form */}
-        <form className="p-6 tlex flex0col max-w-sm gap-3" onSubmit={handleSubmit}>
+        {/* <form className="p-6 tlex flex0col max-w-sm gap-3" onSubmit={handleSubmit}>
           <input onChange={handleInputChange} type="text" placeholder="problem id" name="id" />
           <input onChange={handleInputChange} type="text" placeholder="title" name="title" />
           <input onChange={handleInputChange} type="text" placeholder="difficulty" name="difficulty" />
@@ -80,7 +103,7 @@ export default function Home() {
           <input onChange={handleInputChange} type="text" placeholder="videoId?" name="videoId" />
           <input onChange={handleInputChange} type="text" placeholder="link?" name="link" />
           <button className="bg-white">save to database</button>
-        </form>
+        </form> */}
       </main>
     </>
   )
