ed4459f53ab6417487e9aab237bb3219e73b7841
Patrick Kim <patrick.yrkim@gmail.com>
2023-09-02 19:02:05 -0700

    Add create users functionality

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useSetRecoilState } from 'recoil'
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth"
 import { useRouter } from 'next/router'
-import { auth } from '@/firebase/firebase'
+import { auth, firestore } from '@/firebase/firebase'
 
 import { authModalState } from '@/atoms/authModalAtom'
+import { doc, setDoc } from 'firebase/firestore'
+import { toast } from 'react-toastify'
 
 type SignupProps = {};
 
@@ -32,67 +34,88 @@ const Signup:React.FC<SignupProps> = () => {
 		if (!inputs.email || !inputs.password || !inputs.displayName) return alert("Please fill all fields")
 
 		try {
+            toast.loading("Creating your account", { position: "top-center", toastId: "loadingToast" })
+
 			const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password)
 			if (!newUser) return
+
+            const userData = {
+                uid: newUser.user.uid, 
+                email: newUser.user.email,
+                displayName: inputs.displayName,
+                createdAt: Date.now(),
+                updatedAt: Date.now(),
+                likedProblems: [],
+                dislikedProblems: [],
+                solvedProblems: [],
+                starredProblems: [],
+            }
+
+            await setDoc(doc(firestore, "users", newUser.user.uid), userData)
 			router.push("/")
 		} catch (error: any) {
-            alert(error.message)
-		} 
+            // alert(error.message)
+            toast.error(error.message, { position: "top-center" })
+		} finally {
+            toast.dismiss("loadingToast")
+        }
 	}
 
-    return <form className='space-y-6 px-6 pb-4' onSubmit={handleRegister}>
-        <h3 className='text-xl font-medium text-white'>register</h3>
-        <div>
-            <label htmlFor="email" className='text-sm font-medium block mb-2 text-gray-300'>
-                Email
-            </label>
-            <input
-                onChange={handleChangeInput} 
-                type="email" 
-                name='email' 
-                id='email' 
-                className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
-                placeholder='name@email.com'
-            />
-        </div>
-        <div>
-            <label htmlFor="name" className='text-sm font-medium block mb-2 text-gray-300'>
-                Name
-            </label>
-            <input
-                onChange={handleChangeInput} 
-                type="name" 
-                name='name' 
-                id='name' 
-                className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
-                placeholder='John Doe'
-            />
-        </div>
-        <div>
-            <label htmlFor="password" className='text-sm font-medium block mb-2 text-gray-300'>
-                Password
-            </label>
-            <input
-                onChange={handleChangeInput} 
-                type="password" 
-                name='password' 
-                id='password' 
-                className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
-                placeholder='******'
-            />
-        </div>
-        <button 
-            type='submit' 
-            className='w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'
-        >
-            {loading ? "Registering..." : "Register"}
-        </button>
-        <div className='text-sm font-medium text-gray-300'>
-            Already have an account?{" "}
-            <a href="#" className='text-blue-700 hover:underline' onClick={handleClick}>
-                Log In
-            </a>
-        </div>
-    </form>
+    return (
+        <form className='space-y-6 px-6 pb-4' onSubmit={handleRegister}>
+            <h3 className='text-xl font-medium text-white'>register</h3>
+            <div>
+                <label htmlFor="email" className='text-sm font-medium block mb-2 text-gray-300'>
+                    Email
+                </label>
+                <input
+                    onChange={handleChangeInput} 
+                    type="email" 
+                    name='email' 
+                    id='email' 
+                    className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
+                    placeholder='name@email.com'
+                />
+            </div>
+            <div>
+                <label htmlFor="name" className='text-sm font-medium block mb-2 text-gray-300'>
+                    Name
+                </label>
+                <input
+                    onChange={handleChangeInput} 
+                    type="name" 
+                    name='name' 
+                    id='name' 
+                    className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
+                    placeholder='John Doe'
+                />
+            </div>
+            <div>
+                <label htmlFor="password" className='text-sm font-medium block mb-2 text-gray-300'>
+                    Password
+                </label>
+                <input
+                    onChange={handleChangeInput} 
+                    type="password" 
+                    name='password' 
+                    id='password' 
+                    className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' 
+                    placeholder='******'
+                />
+            </div>
+            <button 
+                type='submit' 
+                className='w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'
+            >
+                {loading ? "Registering..." : "Register"}
+            </button>
+            <div className='text-sm font-medium text-gray-300'>
+                Already have an account?{" "}
+                <a href="#" className='text-blue-700 hover:underline' onClick={handleClick}>
+                    Log In
+                </a>
+            </div>
+        </form>
+    )
 }
 export default Signup
