5eae14163ed76b8cd5a1b72d7f23925f6175686d
Patrick Kim <patrick.yrkim@gmail.com>
2023-09-14 15:29:55 -0700

    Add react confetti

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Split from 'react-split';
+import Confetti from 'react-confetti';
 
 import ProblemDescription from './ProblemDescription/ProblemDescription';
 import Playground from './Playground/Playground';
 import { Problem } from '@/utils/types/problem';
+import useWindowSize from '@/hooks/useWindowSize';
 
 type WorkspaceProps = {
     problem: Problem
 };
 
 const Workspace:React.FC<WorkspaceProps> = ({ problem }) => {
+    const { width, height } = useWindowSize()
+    const [success, setSuccess] = useState(false)
+
     return <Split className='split' minSize={0}>
         <ProblemDescription problem={problem} />
         <div className='bg-dark-fill-2'>
             <Playground problem={problem} />
+            {success && (
+                <Confetti 
+                    gravity={0.3}
+                    tweenDuration={4000}
+                    width={width - 1}
+                    height={height - 1}
+                />
+            )}
         </div>
     </Split>
 }
